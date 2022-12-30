5d2121bd66ea850e99f07cd8b691af949c6b9eb6
u18303221 <112337357+u18303221@users.noreply.github.com>
2022-12-30 17:45:03 -0500

    Implemtacion de formulario reactivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component, VERSION } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  formulario: FormGroup;
+  public isEnviarDatos: boolean = false;
+  constructor(private fb: FormBuilder) {
+    this.crearFomrulario();
+  }
+
+  crearFomrulario() {
+    this.formulario = this.fb.group({
+      nombres: ['', [Validators.required]],
+      dni: [
+        '',
+        [Validators.required, Validators.maxLength(8), Validators.minLength(8)],
+      ],
+      categoria: ['', [Validators.required]],
+      comentario: ['', [Validators.required]],
+    });
+  }
+
+  enviarDatos() {
+    if (this.formulario.invalid) {
+      //mostrar alerta
+      console.log('Ingrese los datos requeridos');
+      return;
+    }
+    this.isEnviarDatos = true;
+  }
 }
